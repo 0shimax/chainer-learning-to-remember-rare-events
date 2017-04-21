ec3d0d959055f6edf98d393b865b8d3a55f2f89d
0shimax <shin.keeper@gmail.com>
2017-04-21 20:06:36 +0900

    replace wrong files

---
+++ src/net/squeeze_net_external_long_term_memory.py
@@ -0,0 +1,154 @@
+import numpy as np
+import chainer
+from chainer import cuda
+import chainer.functions as F
+import chainer.links as L
+import sys
+sys.path.append('./src/common/loss_functions')
+sys.path.append('./src/common/linker')
+sys.path.append('./src/common/functions')
+from memory_loss import memory_loss
+from pos_neg_idxs_extractor import pos_neg_idxs_extractor
+from external_long_term_memory import ExternalLongTermMemory
+from update_memory import update_memory
+from external_memory_accuracy import external_memory_accuracy
+
+
+class Fire(chainer.Chain):
+    def __init__(self, in_size, s1, e1, e3):
+        super().__init__(
+            conv1=L.Convolution2D(in_size, s1, 1),
+            conv2=L.Convolution2D(s1, e1, 1),
+            conv3=L.Convolution2D(s1, e3, 3, pad=1),
+        )
+
+    def __call__(self, x):
+        h = F.elu(self.conv1(x))
+        h_1 = self.conv2(h)
+        h_3 = self.conv3(h)
+        h_out = F.concat([h_1, h_3], axis=1)
+        return F.elu(h_out)
+
+
+class SqueezeNetExternalLongTermMemory(chainer.Chain):
+    version = '0.1.0'
+    alpha = 0.1
+    def __init__(self, n_class, in_ch, update_weight=0.5, vec_size=4096):
+        super().__init__(
+            conv1=L.Convolution2D(in_ch, 96, 7, stride=2, pad=3),
+            fire2=Fire(96, 16, 64, 64),
+            fire3=Fire(128, 16, 64, 64),
+            fire4=Fire(128, 16, 128, 128),
+            fire5=Fire(256, 32, 128, 128),
+            fire6=Fire(256, 48, 192, 192),
+            fire7=Fire(384, 48, 192, 192),
+            fire8=Fire(384, 64, 256, 256),
+            fire9=Fire(512, 64, 256, 256),
+
+            conv9=L.Convolution2D(512*21, vec_size, 1, pad=0),  # *21
+            conv_infer=L.Convolution2D(vec_size, n_class, 1, pad=0),
+            apply_memory=ExternalLongTermMemory(n_class, vec_size),
+        )
+
+        self.train = True
+        self.n_class = n_class
+        self.active_learn = False
+        self.update_weight = update_weight
+        self.memory = None
+        self.memory_value = None
+        self.memory_age = None
+
+    def clear(self):
+        self.loss = 0
+        self.accuracy = None
+        if self.memory is None:
+            self.memory = chainer.Variable(self.apply_memory.memory.data, volatile=True)
+            self.memory_value = chainer.Variable(self.apply_memory.memory_value.data, volatile=True)
+            self.memory_age = chainer.Variable(self.apply_memory.memory_age.data, volatile=True)
+
+    def __call__(self, x, t=None):
+        self.clear()
+        x.volatile = not self.train
+        t.volatile = 'AUTO'
+
+        # print("init memory")
+        # print(self.memory_value.data)
+        if self.train:
+            self.apply_memory.memory.data = self.memory.data.copy()
+            self.apply_memory.memory_value.data = self.memory_value.data.copy()
+            self.apply_memory.memory_age.data = self.memory_age.data.copy()
+
+        h = F.elu(self.conv1(x))
+        h = F.max_pooling_2d(h, 3, stride=2)
+
+        h = self.fire2(h)
+        h = self.fire3(h)
+        h = self.fire4(h)
+
+        h = F.max_pooling_2d(h, 3, stride=2)
+
+        h = self.fire5(h)
+        h = self.fire6(h)
+        h = self.fire7(h)
+        h = self.fire8(h)
+
+        h = F.spatial_pyramid_pooling_2d(h, 3, F.MaxPooling2D)
+        h = F.elu(self.conv9(h))
+
+        query = F.reshape(h, h.data.shape[:2])
+
+        # using external memory from here
+        normed_query = F.normalize(query)
+
+        self.memory = chainer.Variable( \
+            self.apply_memory.memory.data, volatile='AUTO')
+        self.memory_value = chainer.Variable( \
+            self.apply_memory.memory_value.data, volatile='AUTO')
+        self.memory_age = chainer.Variable( \
+            self.apply_memory.memory_age.data, volatile='AUTO')
+
+        all_similarities = F.matmul( \
+            normed_query, self.memory, transb=True)  # (n_batch, memory_size)
+        all_similarities.volatile = 'AUTO'
+
+        with chainer.no_backprop_mode():
+            # hint_pool_idxs: (n_batch, n_best)
+            # infered_class: (n_batch)
+            hint_pool_idxs, infered_class = \
+                self.apply_memory(all_similarities, self.memory, self.memory_value)
+
+            teacher_idxs, neg_idxs, incorrect_memory_lookup = \
+                pos_neg_idxs_extractor(self.memory.data,
+                            self.memory_value.data,
+                            hint_pool_idxs.data,
+                            all_similarities,
+                            t)
+
+        n_batch = len(t.data)
+        teacher_vals = F.select_item(all_similarities, teacher_idxs)
+        neg_teacher_vals = F.select_item(all_similarities, neg_idxs)
+
+        self.loss = \
+            F.relu(neg_teacher_vals - teacher_vals + self.alpha) \
+            - self.alpha
+        self.loss = F.sum(self.loss)
+        self.loss /= n_batch
+
+        if self.train:
+            hint_pool_idxs.volatile = 'AUTO'
+            normed_query.volatile = 'AUTO'
+            with chainer.no_backprop_mode():
+                self.memory, self.memory_value, self.memory_age = \
+                    update_memory( \
+                        normed_query, teacher_idxs, incorrect_memory_lookup,
+                        t, self.memory.data, \
+                        self.memory_value.data, self.memory_age.data)
+                self.memory = F.normalize(self.memory)
+                self.apply_memory.memory.data = self.memory.data.copy()
+                self.apply_memory.memory_value.data = self.memory_value.data.copy()
+                self.apply_memory.memory_age.data = self.memory_age.data.copy()
+
+        with chainer.no_backprop_mode():
+            self.accuracy = external_memory_accuracy(infered_class, t)
+        chainer.report({'loss': self.loss, 'accuracy': self.accuracy}, self)
+        return self.loss
---
+++ src/net/squeeze_net_external_memory.py
@@ -0,0 +1,117 @@
+import numpy as np
+import chainer
+from chainer import cuda
+import chainer.functions as F
+import chainer.links as L
+import sys
+sys.path.append('./src/common/loss_functions')
+sys.path.append('./src/common/active_learning_tools')
+sys.path.append('./src/common/linker')
+from median_weighted_softmax_cross_entropy import median_weighted_softmax_cross_entropy
+from al_operator_in_net import mask_gt_for_active_learning
+from memory_loss import memory_loss
+from external_memory import ExternalMemory
+import subprocess
+
+
+def exists_gpu():
+    cmd ='lspci | grep VGA | cut -d" " -f 1'
+    process = subprocess.Popen(cmd,
+                                shell=True,
+                                stdout=subprocess.PIPE,
+                                stderr=subprocess.PIPE)
+    b_out, b_err = process.communicate()
+    return len(b_out)>0
+
+
+class Fire(chainer.Chain):
+    def __init__(self, in_size, s1, e1, e3):
+        super().__init__(
+            conv1=L.Convolution2D(in_size, s1, 1),
+            conv2=L.Convolution2D(s1, e1, 1),
+            conv3=L.Convolution2D(s1, e3, 3, pad=1),
+        )
+
+    def __call__(self, x):
+        h = F.elu(self.conv1(x))
+        h_1 = self.conv2(h)
+        h_3 = self.conv3(h)
+        h_out = F.concat([h_1, h_3], axis=1)
+        return F.elu(h_out)
+
+
+class SqueezeNetExternalMemory(chainer.Chain):
+    version = '0.1.0'
+    def __init__(self, n_class, in_ch, update_weight=0.5, memory_size=4096):
+        super().__init__(
+            conv1=L.Convolution2D(in_ch, 96, 7, stride=2, pad=3),
+            fire2=Fire(96, 16, 64, 64),
+            fire3=Fire(128, 16, 64, 64),
+            fire4=Fire(128, 16, 128, 128),
+            fire5=Fire(256, 32, 128, 128),
+            fire6=Fire(256, 48, 192, 192),
+            fire7=Fire(384, 48, 192, 192),
+            fire8=Fire(384, 64, 256, 256),
+            fire9=Fire(512, 64, 256, 256),
+
+            conv9=L.Convolution2D(512*21, memory_size, 1, pad=0),  # *21
+            conv_infer=L.Convolution2D(memory_size, n_class, 1, pad=0),
+            apply_memory=ExternalMemory( \
+                n_class, memory_size),
+        )
+
+        self.train = True
+        self.n_class = n_class
+        self.active_learn = False
+        self.update_weight = update_weight
+
+    def clear(self):
+        self.loss = 0
+        self.accuracy = None
+
+    def __call__(self, x, t=None):
+        self.clear()
+        x.volatile = not self.train
+        t.volatile = 'auto'
+
+        h = F.elu(self.conv1(x))
+        h = F.max_pooling_2d(h, 3, stride=2)
+
+        h = self.fire2(h)
+        h = self.fire3(h)
+        h = self.fire4(h)
+
+        h = F.max_pooling_2d(h, 3, stride=2)
+
+        h = self.fire5(h)
+        h = self.fire6(h)
+        h = self.fire7(h)
+        h = self.fire8(h)
+
+        h = F.spatial_pyramid_pooling_2d(h, 3, F.MaxPooling2D)
+        h = F.elu(self.conv9(h))
+
+        memory_h = chainer.Variable(h.data, volatile='auto')
+        with chainer.no_backprop_mode():
+            weight, self.memory = \
+                self.apply_memory(memory_h, t, self.update_weight, self.train)
+
+        if self.train:
+            self.apply_memory.memory.data = self.memory.data
+
+        h = F.dropout(h, ratio=0.5, train=self.train)
+        h = self.conv_infer(h)
+        h = F.reshape(h, (-1, self.n_class))
+
+        h = h*weight
+        self.h = h
+        self.prob = F.softmax(h)
+
+        if self.active_learn:
+            t = mask_gt_for_active_learning(self.prob, t, self.xp, self.n_class)
+
+        self.loss = F.softmax_cross_entropy(h, t)
+
+        self.accuracy = F.accuracy(h, t)
+        chainer.report({'loss': self.loss, 'accuracy': self.accuracy}, self)
+        return self.loss
